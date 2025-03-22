53fae939857267697cb3eedc4b93d432a0da7703
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-03-22 09:32:50 +0530

    qr code

--- day3.py
+++ day3.py
@@ -8,12 +8,12 @@ def generate_qr(data, filename="qrcode.png"):
         box_size=10,
         border=4
     )
-    # qr.add_data(data)
-    # qr.make(fit=True)
+    qr.add_data(data)
+    qr.make(fit=True)
 
-    img = qr.make_image(fill="black", back_color="white")
-    img.save(filename)
-    print(f"QR Code saved as {filename}")
+    # img = qr.make_image(fill="black", back_color="white")
+    # img.save(filename)
+    # print(f"QR Code saved as {filename}")
 
 # Get user input
 text = input("Enter text or URL to generate QR Code: ")
