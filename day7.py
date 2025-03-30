80e0cdc16726ee81af461b8c678d8bbe3ab199da
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-03-30 11:55:33 +0530

    movie recommendations

--- day7.py
+++ day7.py
@@ -27,19 +27,21 @@ def get_movie_recommendations(movie_name, movies_df, similarity_matrix):
     """Returns a list of recommended movies based on a given movie."""
     if movie_name not in movies_df['Movie'].values:
         return "Movie not found in the database."
-    # movie recomend system
+    
     index = movies_df[movies_df['Movie'] == movie_name].index[0]
     scores = list(enumerate(similarity_matrix[index]))
     scores = sorted(scores, key=lambda x: x[1], reverse=True)
     recommended_movies = [movies_df.iloc[i[0]]['Movie'] for i in scores[1:6]]
     
     return recommended_movies
+
 def main():
     movies_df = load_movie_data()
     similarity_matrix = compute_similarity(movies_df)
     
     print("Available Movies:")
     print(movies_df[['Movie', 'Genre']].to_string(index=False))
+    
     movie_name = input("Enter a movie name for recommendations: ")
     recommendations = get_movie_recommendations(movie_name, movies_df, similarity_matrix)
     
