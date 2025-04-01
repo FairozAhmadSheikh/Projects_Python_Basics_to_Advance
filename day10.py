46795d793e8fc70f61e9a262fb92f653d201c8ed
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-04-01 11:37:00 +0530

    c2

--- day10.py
+++ day10.py
@@ -1,3 +1,14 @@
 import numpy as np
 import random
 import time
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+def generate_dataset(samples=1000):
+    """Generates a dataset for XOR operation."""
+    X = np.random.randint(0, 2, (samples, 2))
+    y = np.array([a ^ b for a, b in X])
+    return X, y.reshape(-1, 1)
