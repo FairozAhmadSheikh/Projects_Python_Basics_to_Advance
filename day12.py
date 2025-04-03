1c609262945e9d9d0567e3c502cdcc7696666ae9
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-04-03 07:13:03 +0530

    c4

--- day12.py
+++ day12.py
@@ -13,3 +13,7 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
+# Initialize screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Snake Game")
+clock = pygame.time.Clock()
