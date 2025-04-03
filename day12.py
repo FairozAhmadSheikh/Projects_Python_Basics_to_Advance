482a688657037c917410e7c3f438b10bcc784880
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-04-03 07:13:13 +0530

    c5

--- day12.py
+++ day12.py
@@ -17,3 +17,17 @@ BLACK = (0, 0, 0)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Snake Game")
 clock = pygame.time.Clock()
+class SnakeGame:
+    def __init__(self):
+        self.snake = [(100, 100), (90, 100), (80, 100)]
+        self.direction = (GRID_SIZE, 0)
+        self.food = self.spawn_food()
+        self.running = True
+        self.score = 0
+
+    def spawn_food(self):
+        while True:
+            food = (random.randint(0, (WIDTH // GRID_SIZE) - 1) * GRID_SIZE,
+                    random.randint(0, (HEIGHT // GRID_SIZE) - 1) * GRID_SIZE)
+            if food not in self.snake:
+                return food
