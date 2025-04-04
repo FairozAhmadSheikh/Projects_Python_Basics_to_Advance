2219324e84f5e149bc026d66e83fb290dd7667cb
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-04-04 11:50:56 +0530

    pygame

--- day13.py
+++ day13.py
@@ -20,3 +20,23 @@ BLACK = (0, 0, 0)
 player_img = pygame.image.load("player.png")
 enemy_img = pygame.image.load("enemy.png")
 bullet_img = pygame.image.load("bullet.png")
+
+# Player class
+class Player:
+    def __init__(self):
+        self.x = WIDTH // 2
+        self.y = HEIGHT - 80
+        self.speed = 5
+        self.bullets = []
+
+    def move(self, direction):
+        self.x += direction * self.speed
+        self.x = max(0, min(WIDTH - 64, self.x))
+
+    def shoot(self):
+        self.bullets.append(Bullet(self.x + 16, self.y))
+
+    def draw(self):
+        screen.blit(player_img, (self.x, self.y))
+        for bullet in self.bullets:
+            bullet.draw()
