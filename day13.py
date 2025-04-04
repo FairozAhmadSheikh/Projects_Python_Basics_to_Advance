ff31f6cbb0bb01c803b7073e576abcd86885fd47
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-04-04 11:50:24 +0530

    pygame

--- day13.py
+++ day13.py
@@ -9,3 +9,9 @@ pygame.init()
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Shooter Game")
+
+# Colors
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
