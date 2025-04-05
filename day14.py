88ecb3f6b49d87f502ad20376c69c587641222b8
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-04-05 10:15:41 +0530

    c2

--- day14.py
+++ day14.py
@@ -10,3 +10,15 @@ BUFFER_SIZE = 4096
 
 clients = []
 usernames = []
+# Handle individual client connection
+def handle_client(client):
+    while True:
+        try:
+            message = client.recv(BUFFER_SIZE).decode('utf-8')
+            if message.startswith('FILE_SEND'):
+                filename, filesize = message.split(SEPARATOR)[1:]
+                filename = os.path.basename(filename)
+                filesize = int(filesize)
+                with open("received_" + filename, "wb") as f:
+                    while filesize > 0:
+                        
