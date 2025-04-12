2c5cfcb84f6806a9940793f8b419b599b2db1dc1
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-04-12 05:48:28 +0530

    c2

--- day20.py
+++ day20.py
@@ -2,3 +2,15 @@ from cryptography.fernet import Fernet
 import json
 import os
 import getpass
+def generate_key():
+    key = Fernet.generate_key()
+    with open(KEY_FILE, "wb") as file:
+        file.write(key)
+
+def load_key():
+    return open(KEY_FILE, "rb").read()
+
+if not os.path.exists(KEY_FILE):
+    generate_key()
+
+fernet = Fernet(load_key())
