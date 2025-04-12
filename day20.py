1d77284ee1d593bc3d90d1680fe6d8dfaced353a
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-04-12 05:48:49 +0530

    c3

--- day20.py
+++ day20.py
@@ -14,3 +14,13 @@ if not os.path.exists(KEY_FILE):
     generate_key()
 
 fernet = Fernet(load_key())
+
+def load_data():
+    if not os.path.exists(DATA_FILE):
+        return {}
+    with open(DATA_FILE, "r") as file:
+        return json.load(file)
+
+def save_data(data):
+    with open(DATA_FILE, "w") as file:
+        json.dump(data, file, indent=4)
