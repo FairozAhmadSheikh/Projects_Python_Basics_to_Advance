4706391d24c5c79a8b2085eb18a1d833a7049a38
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-04-22 17:07:42 +0530

    c4

--- day27.py
+++ day27.py
@@ -15,3 +15,15 @@ def is_valid(board, row, col, num):
             if board[i][j] == num:
                 return False
     return True
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
