82e06c014ee8978e44bb34b1577b5e6a20362b07
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-04-28 18:43:30 +0530

    c5

--- day34.py
+++ day34.py
@@ -50,3 +50,32 @@ def receive():
         thread.start()
 
 receive()
+
+
+# Client Code 
+
+import socket
+import threading
+
+# Server settings
+HOST = '127.0.0.1'
+PORT = 55555
+
+nickname = input("Choose your nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+# Listen to server
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NICK':
+                client.send(nickname.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print("❌ An error occurred!")
+            client.close()
+            break
