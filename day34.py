6b431fa619151b094d871455389ebfe596bda4b3
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-04-28 18:42:55 +0530

    c3

--- day34.py
+++ day34.py
@@ -30,3 +30,23 @@ def handle(client):
             broadcast(f'🚪 {nickname} left the chat!'.encode('utf-8'))
             nicknames.remove(nickname)
             break
+# Receive connections
+def receive():
+    print('🖥️ Server is running...')
+    while True:
+        client, address = server.accept()
+        print(f"✅ Connected with {str(address)}")
+
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"👤 Nickname: {nickname}")
+        broadcast(f'📢 {nickname} joined the chat!'.encode('utf-8'))
+        client.send('🛡️ Connected to the server!'.encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+receive()
