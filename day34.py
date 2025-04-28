16cc3918388b8b5375390d0bb829c52f33dfb380
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-04-28 18:42:26 +0530

    c1

---
+++ day34.py
@@ -0,0 +1,18 @@
+import socket
+import threading
+
+# Server settings
+HOST = '127.0.0.1'  # localhost
+PORT = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+clients = []
+nicknames = []
+
+# Broadcast messages to all clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
