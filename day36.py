b3ab99e240a647c37da487d0c6264078328d5e12
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-04-30 19:50:54 +0530

    c3

--- day36.py
+++ day36.py
@@ -15,3 +15,11 @@ for filename in os.listdir(path):
     img = cv2.imread(f'{path}/{filename}')
     images.append(img)
     names.append(os.path.splitext(filename)[0])
+def find_encodings(images):
+    encode_list = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)
+        if encode:
+            encode_list.append(encode[0])
+    return encode_list
