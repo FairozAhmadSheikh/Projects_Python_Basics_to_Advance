c18fd6beec7ef82110c7e840ca4fdaea6def920d
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-05-02 11:06:37 +0530

    c1

---
+++ day38.py
@@ -0,0 +1,15 @@
+# pip install pdfplumber sklearn sentence-transformers pandas
+import pdfplumber
+import os
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('all-MiniLM-L6-v2')  # Fast & accurate
+
+def extract_text_from_pdf(pdf_path):
+    with pdfplumber.open(pdf_path) as pdf:
+        text = ''
+        for page in pdf.pages:
+            text += page.extract_text() + '\n'
+    return text
