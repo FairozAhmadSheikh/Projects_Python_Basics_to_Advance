acee16425242c1344b1169feaf78d14aecc6a8d2
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-07-07 19:39:41 +0530

    c3

--- Digital Fortune Cookie.py
+++ Digital Fortune Cookie.py
@@ -5,3 +5,10 @@ import webbrowser
 
 #  Each time you run it, it gives you a randomly generated digital "fortune",
 # opens a calming website, and plays a unique ASCII animation.
+fortunes = [
+    "You will debug a bug no one else could.",
+    "Today, your code compiles on the first try.",
+    "A surprise pull request will brighten your day.",
+    "Refactor now, profit later.",
+    "Your logic is as sharp as Python’s syntax.",
+]
