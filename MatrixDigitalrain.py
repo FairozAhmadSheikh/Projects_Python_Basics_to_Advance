5fc4c3694c6dc0c74a362a02db343cb6b930a0c7
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-08-21 21:19:29 +0530

    loop

--- MatrixDigitalrain.py
+++ MatrixDigitalrain.py
@@ -10,3 +10,10 @@ except OSError:
 
 # The characters to use for the "rain"
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;':\",./<>?`~"
+columns = [0] * cols
+
+while True:
+    for i in range(cols):
+        # If the column is at the top of the screen (i.e., inactive)
+        if columns[i] == 0 and random.random() < 0.02:
+            columns[i] = random.randint(10, rows - 5)
