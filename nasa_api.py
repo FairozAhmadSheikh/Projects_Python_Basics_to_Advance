940c5c24a7c80c3d58d12003ce1c9656793049ad
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-10-08 19:12:23 +0530

    typing animation

--- nasa_api.py
+++ nasa_api.py
@@ -2,3 +2,12 @@ import requests  # for making HTTP requests
 import time      # for simulating loading delays
 import sys       # for terminal animations
 import textwrap  # for wrapping long text
+
+
+# function to create a typing animation
+def slow_print(text, delay=0.03):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()
